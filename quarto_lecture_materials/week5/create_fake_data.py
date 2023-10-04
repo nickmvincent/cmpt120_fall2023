853d81e5d2afab526a5b2864d78319f5ae7cea68
Nick Vincent <nickmvincent@gmail.com>
2023-10-04 11:36:52 -0700

    week 5 update

---
+++ quarto_lecture_materials/week5/create_fake_data.py
@@ -0,0 +1,46 @@
+import csv
+import random
+
+# Lists of names and diets
+names = ["Michael Jordan", "Tom Hanks", "Arya Stark", "Goku", "Scarlett Johansson", 
+         "Mickey Mouse", "Hermione Granger", "Naruto", "Serena Williams", "Oprah Winfrey",
+         "Leonardo DiCaprio", "Mario", "Taylor Swift", "Darth Vader", "James Bond",
+         "Usain Bolt", "Tony Stark", "Bugs Bunny", "The Rock", "Walter White",
+         "Sheldon Cooper", "Mulan", "Elsa", "Sherlock Holmes", "Jack Sparrow",
+         "Luffy", "Luke Skywalker", "Marilyn Monroe", "Freddie Mercury", "Lady Gaga",
+         "Superman", "Wonder Woman", "Neo", "Beyonce", "Ash Ketchum",
+         "Lara Croft", "Elvis Presley", "Kermit the Frog", "Spider-Man", "Hannibal Lecter",
+         "Indiana Jones", "Simba", "Rihanna", "Dexter Morgan", "Morpheus",
+         "Wolverine", "Cinderella", "Captain America", "Thor", "Katniss Everdeen"]
+
+movies_by_genre = {
+    "Drama": ["The Shawshank Redemption", "Forrest Gump", "The Godfather", "Titanic", "Fight Club"],
+    "Action": ["The Dark Knight", "Avengers: Endgame", "Pulp Fiction", "Jurassic Park", "Lord of the Rings: The Fellowship of the Ring"],
+    "Sci-Fi": ["Inception", "The Matrix", "Back to the Future", "Star Wars: Episode IV", "Blade Runner"],
+    "Animation": ["The Lion King", "Toy Story", "Frozen", "Finding Nemo", "Shrek"],
+    "Classic": ["Casablanca", "Jaws", "Gone with the Wind", "Psycho", "Citizen Kane"]
+}
+
+parties = ["very left wing", "left wing", "right wing", "very right wing"]
+
+diets = ["plant-heavy", "meat-heavy", "fruit-heavy", "balanced", "dairy-heavy"]
+
+# Generate 50 records
+records = [["name", "favorite_movie", "second_favorite_movie", "preferred_political_party", "ideal_diet"]]
+for _ in range(50):
+    name = random.choice(names)
+    genre = random.choice(list(movies_by_genre.keys()))
+    movie = random.choice(movies_by_genre[genre])
+    movie2 = random.choice(movies_by_genre[genre])
+    party = random.choice(parties)
+    diet = random.choice(diets)
+    records.append([name, movie, movie2, party, diet])
+
+# Write to a CSV file
+with open("fake_data.csv", "w", newline="") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerows(records)
+
+# Display the first few rows
+for row in records[:5]:
+    print(", ".join(row))
---
+++ quarto_lecture_materials/week5/recsys.py
@@ -0,0 +1,44 @@
+# select a user + init variables to keep track of scores
+index_of_user = 17
+top_score = 0
+top_record = ""
+recommendation = ""
+
+# load data
+with open("fake_data.csv", "r") as file:
+    all_lines = file.readlines()
+    header = all_lines[0]
+    records = all_lines[1:]
+
+    user_record = records[index_of_user].strip().split(",")
+
+    # use a loop to go through all other record lines (one record = one user)
+    for record in records:
+        columns = record.strip().split(",")
+
+        if user_record[0] == columns[0]:
+            print("Skipping ", record)
+            continue
+
+        score_for_current_record = 0
+        # split each record line into a list of items
+        
+        # use a nested loop to go through each item
+        # use manually defined [1,3,4,5] to get only
+        # favorite_movie, party, diet (skip)
+        for column_index in [1,3,4]:
+            if columns[column_index] == user_record[column_index]:
+                score_for_current_record += 1
+        
+        if score_for_current_record > top_score:
+            top_score = score_for_current_record
+            top_record = record
+            recommendation = columns[2]
+
+print("You are", user_record)
+
+print("After careful consideration, we have found that the most similar user is")
+print(top_record)
+print(f"You have a similarity score of {top_score}")
+
+print("We recommend you watch", recommendation)
